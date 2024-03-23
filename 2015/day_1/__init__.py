a26479f248c38b812191ad3616df39fc271e2904
sayuj1998 <sayuj21@gmail.com>
2024-03-23 14:20:13 -0600

    2015 Day 1 Part 1

--- 2015/day_1/__init__.py
+++ 2015/day_1/__init__.py
@@ -1,1 +1,13 @@
-print("hi")
+with open("data.txt") as f:
+    data = f.read()
+
+def part_1():
+    start = 0
+    for char in data:
+        if char == "(":
+            start += 1
+        if char == ")":
+            start -= 1
+    return start
+print(part_1())
+
