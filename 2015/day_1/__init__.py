703014f01cf34d1f7ce85b3e51fbb359931be32b
sayuj1998 <164384520+sayuj1998@users.noreply.github.com>
2024-03-23 15:09:38 -0600

    2015 day 1 (#1)
    * 2015 Day 1 Part 1
    
    * 2015 Day 1 Part 1
    
    * Applying suggestions

--- 2015/day_1/__init__.py
+++ 2015/day_1/__init__.py
@@ -1,1 +1,24 @@
-print("hi")
+with open("data.txt") as f:
+    data = f.read()
+
+def part_1():
+    start = 0
+    for char in data:
+        if char == "(":
+            start += 1
+        elif char == ")":
+            start -= 1
+    return start
+print(part_1())
+
+def part_2():
+    current_floor = 0
+    for index in range(len(data)):
+        if data[index] == ")":
+            current_floor -= 1
+            if current_floor == -1:
+                return index + 1
+        elif data[index] == "(":
+            current_floor += 1
+
+print(part_2())
